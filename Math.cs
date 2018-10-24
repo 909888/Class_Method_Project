902122aa2852d832af3e1ca1d930a64c3a4c10be
ed.lopez1905@gmail.com <ed.lopez1905@gmail.com>
2018-10-23 19:22:07 -0700

    upload

---
+++ Math.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math_Class_Project
+{
+    public class Math
+    {
+        
+        //method 1. 
+        public static int MathMult(int mult1)
+        {
+            int multiply = mult1 * 10;
+            //Console.WriteLine(multiply);
+            return multiply;
+            
+        }
+
+        //method 2.
+        public int MathAdd(int mult1, int mult2)
+        {
+            int multiply = mult1 * mult2;
+            //Console.WriteLine(multiply);
+            return multiply;
+
+        }
+
+        //method 3.
+        public int MathSub(int mult1, int mult2, int mult3)
+        {
+            int multiply = mult1 * mult2 - mult3;
+            return multiply;
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math_Class_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+            Console.WriteLine("Type a number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            int result =Math.MathMult(num1);
+            Console.WriteLine(result);
+
+            Math mathObject = new Math();
+            int result2 = mathObject.MathAdd(num1, num1);
+            Console.WriteLine(result2);
+
+            Math mathObject2 = new Math();
+            int result3 = mathObject.MathSub(num1, num1, num1);
+            Console.WriteLine(result2);
+
+            Console.ReadLine();
+
+        }
+
+        
+    }
+}
